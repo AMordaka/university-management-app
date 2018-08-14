f2d4aaff5bdda027de955e69525cc41744c0810a
Arkadiusz Mordaka <1995.arek.m@gmail.com>
2018-08-14 22:25:01 +0200

    Zmiana portu heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
