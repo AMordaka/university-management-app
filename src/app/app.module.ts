39ba31c634860a8f249c9e5aa59b9e68c4a24909
Arkadiusz Mordaka <1995.arek.m@gmail.com>
2018-08-24 20:05:51 +0200

    Dodanie do modułu zależności

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,34 @@ import {NgModule} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {ReactiveFormsModule} from '@angular/forms';
 
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {UiModule} from './ui/ui.module';
 import {ModalComponent} from './modal/modal.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {AuthGuard} from './guards/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModalComponent
+    ModalComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     UiModule,
     NgbModule.forRoot(),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {
