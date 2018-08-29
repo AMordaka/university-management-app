06c62ef692dac805d0a6873028e3f87175714043
Arkadiusz Mordaka <1995.arek.m@gmail.com>
2018-08-29 09:01:43 +0200

    Poprawka w auth service

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -14,7 +14,7 @@ export class AuthenticationService {
 
   login(username: string, password: string) {
     return this.http.post(`${this.API_URL}/api/auth/signin`, {usernameOrEmail: username, password: password}).pipe(map(user => {
-        if (user && user.accessToken) {
+        if (user) {
           localStorage.setItem('currentUser', JSON.stringify(user));
         }
         return user;
