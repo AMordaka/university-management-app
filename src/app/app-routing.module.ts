e53696977d5ca1df66c383ee6bf44152f4bb615c
Arkadiusz Mordaka <1995.arek.m@gmail.com>
2018-08-31 10:38:38 +0200

    Import Angular6 DataTable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import {RegisterComponent} from './register/register.component';
 import {AuthGuard} from './guards/auth.guard';
 import {AboutComponent} from './about/about.component';
 import {ContactComponent} from './contact/contact.component';
+import {UserListComponent} from './user-list/user-list.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'user-list', component: UserListComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {ReactiveFormsModule} from '@angular/forms';
 
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-
+import {DataTableModule} from "angular-6-datatable";
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {UiModule} from './ui/ui.module';
@@ -16,6 +16,7 @@ import {AuthGuard} from './guards/auth.guard';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { AlertComponent } from './directives/alert/alert.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
@@ -26,12 +27,14 @@ import { AlertComponent } from './directives/alert/alert.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    AlertComponent
+    AlertComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     UiModule,
+    DataTableModule,
     NgbModule.forRoot(),
     ReactiveFormsModule,
     HttpClientModule,
