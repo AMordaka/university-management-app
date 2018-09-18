4b0b8e86ed7fcfc389d43fd81cb65ef078d474d6
Arkadiusz Mordaka <arkadiusz1057@gmail.com>
2018-09-18 21:09:08 +0200

    Dodanie Item Service

---
+++ src/app/models/courseInfo.ts
@@ -0,0 +1,5 @@
+export class CourseInfo {
+  studentUsername: string;
+  teacherUsername: string;
+  grade: string;
+}
---
+++ src/app/services/item.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CourseInfo } from '../models/courseInfo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  API_URL = 'https://university-management-app-back.herokuapp.com';
+
+  constructor(private http: HttpClient) {
+  }
+
+  putGrade(courseInfo: CourseInfo) {
+    return this.http.post(`${this.API_URL}/2/putGrade`, courseInfo);
+  }
+
+  createCourse(teacherUsername: string, courseName: string) {
+    return this.http.post(`${this.API_URL}/teacher/${teacherUsername}`, courseName);
+  }
+}
