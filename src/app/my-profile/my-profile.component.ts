b13f616af9b9d5723b845e9d5f2ae45890ded45f
Arkadiusz Mordaka <arkadiusz1057@gmail.com>
2018-10-02 19:42:57 +0200

    Przygotowanie komponentu do uploadu avataru

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { AuthenticationService } from '../services/authentication.service';
 import { AlertService } from '../services/alert.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-my-profile',
@@ -11,8 +12,9 @@ import { AlertService } from '../services/alert.service';
 export class MyProfileComponent implements OnInit {
 
   model: any = {};
+  imgsrc = 'https://c.staticblitz.com/assets/client/components/SideMenu/blitz_logo-11cebad97cad4b50bc955cf72f532d1b.png';
 
-  constructor(private userService: UserService, private authenticationService: AuthenticationService, private alertService: AlertService) {
+  constructor(private userService: UserService, private authenticationService: AuthenticationService, private alertService: AlertService, public _d: DomSanitizer) {
   }
 
   ngOnInit() {
@@ -36,5 +38,10 @@ export class MyProfileComponent implements OnInit {
         console.log(error);
         this.alertService.error(error.error.message);
       });
+  }
+
+  fileChange(e) {
+    const file = e.srcElement.files[0];
+    this.imgsrc = window.URL.createObjectURL(file);
   }
 }
