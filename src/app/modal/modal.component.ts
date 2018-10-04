3364ef5399f27d914a499dcef184a98af04ba721
Arkadiusz Mordaka <arkadiusz1057@gmail.com>
2018-10-04 21:24:11 +0200

    Możliwość edytowania usera przez admina

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from '../models/user';
+import {Component, Input, OnInit} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {User} from '../models/user';
+import {AlertService} from '../services/alert.service';
+import {UserService} from '../services/user.service';
 
 
 @Component({
@@ -11,41 +12,30 @@ import { User } from '../models/user';
 })
 export class ModalComponent implements OnInit {
 
-  editForm: FormGroup;
   loading = false;
   submitted = false;
 
   @Input() user: User;
 
-  constructor(public activeModal: NgbActiveModal, private formBuilder: FormBuilder) {
+  constructor(public activeModal: NgbActiveModal, private alertService: AlertService, private userService: UserService) {
   }
 
   ngOnInit(): void {
-    this.editForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
-      username: ['', [Validators.required, Validators.minLength(6)]],
-      email: ['', Validators.required]
-    });
   }
 
 
   onSubmit() {
     this.submitted = true;
-    console.log(this.editForm.value);
     this.loading = true;
-    /*    this.userService.register(this.editForm.value)
-          .pipe(first())
-          .subscribe(
-            data => {
-              this.alertService.success('Updated successful', true);
-            },
-            error => {
-              console.log(error);
-              this.alertService.error(error.error.message);
-              this.loading = false;
-            });*/
+    console.log(this.user);
+    this.userService.update(this.user).subscribe(
+      data => {
+        this.alertService.success('Updated successful', true);
+        this.loading = false;
+      },
+      error => {
+        this.loading = false;
+        this.alertService.error(error.error.message);
+      });
+    this.activeModal.close();
   }
-
-
-}
