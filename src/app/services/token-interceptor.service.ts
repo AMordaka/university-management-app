29280999927b4352fa2877b681991b2c5929035f
Arkadiusz Mordaka <arkadiusz1057@gmail.com>
2018-10-27 17:12:12 +0200

    Token Interceptor - implementacja

---
+++ src/app/services/token-interceptor.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService implements HttpInterceptor{
+
+  constructor(public authService: AuthenticationService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    request = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.authService.user.accessToken}`
+      }
+    });
+    return next.handle(request);
+  }
+}
